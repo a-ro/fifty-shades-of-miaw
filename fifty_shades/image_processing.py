df6c8da3fe436a5cff5b4070560ca1b25c6935fe
amelie <amelie.rolland.1@gmail.com>
2019-12-24 13:44:21 -0500

    add load and save image tensor

---
+++ fifty_shades/image_processing.py
@@ -0,0 +1,17 @@
+from os.path import join
+
+import tensorflow as tf
+from keras_preprocessing.image import array_to_img
+
+
+def load_image_tensor(image_path: str, max_dim: int = 512) -> tf.Tensor:
+    image_tensor = tf.io.read_file(image_path)
+    image_tensor = tf.image.decode_image(image_tensor, channels=3)
+    image_tensor = tf.image.convert_image_dtype(image_tensor, tf.float32)
+    image_tensor = tf.image.resize(image_tensor, (max_dim, max_dim), preserve_aspect_ratio=True)
+    image_tensor = image_tensor[tf.newaxis, :]
+    return image_tensor
+
+
+def save_image_from_tensor(save_directory: str, file_name: str, tensor_image: tf.Tensor) -> None:
+    array_to_img(tensor_image[0]).save(join(save_directory, file_name))
